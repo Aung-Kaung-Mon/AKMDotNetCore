c1744fa617b60b5b4bec13e41a60cd68e2034dbb
Aung-Kaung-Mon <aungkaungmon.2020.20@gmail.com>
2024-05-03 15:08:01 +0630

    RestApi-Crud-EFCore

---
+++ AKMDotNetCore.RestApi/ConnectionStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AKMDotNetCore.ConsoleApp.Services;
+
+public static class ConnectionStrings
+{
+    public static SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder()
+    {
+        DataSource = "LAPTOP-HA19D87N",
+        InitialCatalog = "AKMDotNetCore",
+        UserID = "sa",
+        Password = "sa@123",
+        TrustServerCertificate = true
+
+    };
+}
+//Scaffold-DbContext "Server=LAPTOP-HA19D87N;Database=AKMDotNetCore;User ID=sa;Password=sa@123;TrustServerCertificate=True;Trusted_Connection=True;" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -Context AKMDotNetCoreContext
---
+++ AKMDotNetCore.RestApi/Controllers/BlogController.cs
@@ -0,0 +1,112 @@
+﻿using AKMDotNetCore.RestApi.DBs;
+using AKMDotNetCore.RestApi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AKMDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+
+        public BlogController()
+        {
+            _context = new AppDbContext();
+        }
+        [HttpGet]
+        public IActionResult Read()
+        {
+            var list = _context.Blogs.ToList();
+            return Ok(list);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Read(int id)
+        {
+            var item = _context.Blogs.FirstOrDefault(x => x.BlogId == id);
+
+            if(item is null)
+
+            {
+                return NotFound("data not found");
+            }
+
+            return Ok(item);
+        }
+
+
+
+        [HttpPost]
+        public IActionResult Create(BlogModel blog)
+        {
+            _context.Blogs.Add(blog);
+            var result =_context.SaveChanges();
+
+            return Ok(result > 0 ? "Created Successfully" : "Fail");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, BlogModel blog)
+        {
+           var item = _context.Blogs.FirstOrDefault(x => x.BlogId ==id);
+            if(item is null)
+            {
+                return NotFound("data not found");
+            }
+
+            item.BlogTitle = blog.BlogTitle;
+            item.BlogArthur = blog.BlogArthur;
+            item.BlogContent = blog.BlogContent;
+            var result = _context.SaveChanges();
+
+            return Ok(result > 0 ? "Updated Successfully" : "Fail");
+
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, BlogModel blog)
+        {
+            var item = _context.Blogs.FirstOrDefault(y => y.BlogId == id);
+            if (item is null)
+            {
+                return NotFound("data not found");
+            }
+
+            if(!string.IsNullOrEmpty(blog.BlogTitle)){
+                item.BlogTitle = blog.BlogTitle;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogArthur))
+            {
+                item.BlogArthur = blog.BlogArthur;
+            }
+
+            if (!string.IsNullOrEmpty(blog.BlogContent))
+            {
+                item.BlogContent = blog.BlogContent;
+            }
+
+            var result = _context.SaveChanges();
+            return Ok(result > 0 ? "Updated Successfully" : "Fail");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+           var blog = _context.Blogs.FirstOrDefault(z => z.BlogId == id);
+
+            if (blog is null)
+            {
+                return NotFound("data not found");
+            }
+
+            _context.Blogs.Remove(blog);
+            var result = _context.SaveChanges();
+
+            return Ok(result > 0 ? "Deleted Successfully" : "Fail");
+        }
+    }
+}
